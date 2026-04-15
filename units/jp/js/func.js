f71905c74092470800da7dae0dfbc6a711cfbde7
DESKTOP-HQ5OOK0\km112 <km11250315@gmail.com>
2026-04-15 22:50:56 +0800

    V1.0.137

--- units/jp/js/func.js
+++ units/jp/js/func.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const vid = document.getElementById("vid");
     if (vid) {
-        vid.textContent = "V1.0.135 更新:04/08";
+        vid.textContent = "V1.0.137 更新:04/15";
     }
 });
 
